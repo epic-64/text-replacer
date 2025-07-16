7c8958e60c057d1836b5bbe6469d707f640e4132
epic-64 <william.raendchen@gmail.com>
2025-07-16 21:14:13 +0200

    Add indoc dependency and implement QuickFix action in the application

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifier
 use ratatui::{prelude::*, widgets::{Block, Borders, Paragraph, Wrap}, DefaultTerminal};
 use regex::Regex;
 
+const ACCENT_COLOR: Color = Color::Red;
+
 trait NiceKeyEvent {
     /// Returns a string representation of the key event for display purposes.
     fn to_nice_string(&self) -> String;
@@ -23,6 +25,7 @@ enum Action {
     RemoveExtraSpaces,
     CopyToClipboard,
     ClearText,
+    QuickFix,
     Exit,
 }
 
@@ -34,6 +37,7 @@ impl Action {
             Action::RemoveExtraSpaces => "Removed extra spaces",
             Action::CopyToClipboard => "Copied text to clipboard",
             Action::ClearText => "Cleared text",
+            Action::QuickFix => "Quick fix applied. Your clipboard was updated.",
             Action::Exit => "Exiting application",
         }
     }
@@ -77,7 +81,7 @@ impl App {
 
 // The user logic for the application.
 impl App {
-    fn copy_text_from_clipboard(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    fn paste_text_from_clipboard(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let mut clipboard = Clipboard::new()?;
         if let Ok(clip_text) = clipboard.get_text() {
             self.text = clip_text;
@@ -107,9 +111,16 @@ impl App {
         self.last_pressed_key = Some(key_event);
 
         let result = match (key_event.code, key_event.modifiers) {
+            (KeyCode::F(1), _) => {
+                self.last_action = Some(Action::QuickFix);
+                self.paste_text_from_clipboard()?;
+                self.remove_extra_spaces();
+                self.copy_text_to_clipboard()?;
+                Ok(())
+            },
             (KeyCode::F(2), _) => {
                 self.last_action = Some(Action::PasteFromClipboard);
-                self.copy_text_from_clipboard()
+                self.paste_text_from_clipboard()
             },
             (KeyCode::F(3), _) => {
                 self.last_action = Some(Action::RemoveExtraSpaces);
@@ -161,16 +172,22 @@ impl Widget for &App {
         // draw the instructions
         let text = "<F2> paste | <F3> remove space | <F4> copy to clipboard | <F5> clear text | <CTRL+c> exit";
         Paragraph::new(text)
-            .block(Block::default().title("Keybinds").borders(Borders::ALL))
+            .block(Block::bordered()
+                .title("Keybinds").title_style(Style::new().fg(ACCENT_COLOR)))
             .render(instructions, buf);
 
         // draw the text from the clipboard
         Paragraph::new(self.text.as_str())
-            .block(Block::default().title("Text Box (press F2 to paste)").borders(Borders::ALL))
+            .block(Block::bordered()
+                .title("Text Box (press F2 to paste)").title_style(Style::new().fg(Color::Red)))
             .wrap(Wrap { trim: false }).render(textbox, buf);
 
         // draw the last pressed key
-        Block::bordered().title("Last Key").render(last_key, buf);
+        Block::bordered()
+            .title("Last Key")
+            .title_style(Style::new().fg(ACCENT_COLOR))
+            .render(last_key, buf);
+
         if let Some(key) = self.last_pressed_key {
             Paragraph::new(key.to_nice_string())
                 .wrap(Wrap { trim: false })
@@ -178,7 +195,12 @@ impl Widget for &App {
         }
 
         // draw the last action taken
-        Block::bordered().title("Last Action").render(last_action, buf);
+        Block::bordered()
+            .title("Last Action")
+            .title_alignment(Alignment::Right)
+            .title_style(Style::new().fg(ACCENT_COLOR))
+            .render(last_action, buf);
+
         if let Some(action) = &self.last_action {
             let action_info = action.as_str();
 
@@ -188,7 +210,10 @@ impl Widget for &App {
         }
 
         // draw the last error message if it exists
-        Block::bordered().title("Last Error").render(last_error, buf);
+        Block::bordered()
+            .title("Last Error")
+            .title_style(Style::new().fg(ACCENT_COLOR))
+            .render(last_error, buf);
         if let Some(ref error) = self.last_error {
             Paragraph::new(error.as_str())
                 .wrap(Wrap { trim: false })
